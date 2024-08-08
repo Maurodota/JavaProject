23eebd4832ac6b520e60cc5f8af6b8da4ed0d57b
Maurodota <maurodota@hotmail.com>
2024-08-08 17:50:36 -0300

    Boletim de notas

---
+++ meu-primeiro-projeto-java/src/edu/dota/primeiraSemana/BoletimEstudandiu.java
@@ -0,0 +1,13 @@
+package edu.dota.primeiraSemana;
+public class BoletimEstudandiu {
+    public static void main(String[] args) {
+        int notaFinal = 7;
+        if(notaFinal <6)
+            System.out.println("Reprovado");
+        else if(notaFinal == 6)
+            System.out.println("Prova de Minerva");
+        else 
+            System.out.println("Meus Parabens voce passou!");
+    }
+    
+}
