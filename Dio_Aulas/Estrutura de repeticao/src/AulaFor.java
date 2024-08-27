86624519ab644f94d33125825d9aa6bc91e64970
Maurodota <maurodota@hotmail.com>
2024-08-27 16:21:36 -0300

    Primeira Aula de "For"

---
+++ Dio_Aulas/Estrutura de repeticao/src/AulaFor.java
@@ -0,0 +1,15 @@
+import java.net.ContentHandlerFactory;
+
+public class AulaFor {
+    public static void main(String[] args) {
+        System.out.println("Contagem para o lançamento ");
+
+        for ( int Contador = 10; Contador >= 0; Contador --){
+        System.out.println(Contador);
+        
+        }
+        System.out.println("LANÇAR");
+
+    }
+    
+}
