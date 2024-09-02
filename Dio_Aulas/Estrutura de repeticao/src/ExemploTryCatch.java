07c5607a567f595784238f7cff6e6ae2f4727378
Maurodota <maurodota@hotmail.com>
2024-09-02 19:00:07 -0300

    Exemplo de "TRY" e "Catch"

---
+++ Dio_Aulas/Estrutura de repeticao/src/ExemploTryCatch.java
@@ -0,0 +1,38 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ExemploTryCatch {
+    public static void main(String[] args) {
+        
+        try{
+        // criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite o sobrenome");
+        String sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
+
+        // imprimindo os dados obtidos pelo usuario
+        System.out.println("Olá, me chama " + nome.toUpperCase() + " ");
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close();
+        }
+        catch (InputMismatchException e){
+           System.out.println("O campo idade e altura precisam ser numericos ");
+        }
+        
+       
+    }
+
+    
+}
